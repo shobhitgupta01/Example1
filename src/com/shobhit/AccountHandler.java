87ba06393c22d847f309ba7df30a9d252e645c05
shobhitgupta01 <shobhit.gupta1995@gmail.com>
2017-07-14 14:03:05 +0530

    Insertion to database

--- src/com/shobhit/AccountHandler.java
+++ src/com/shobhit/AccountHandler.java
@@ -4,11 +4,19 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.io.Console;
+import java.io.IOException;
 import java.util.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 
@@ -38,6 +46,36 @@ public class AccountHandler {
 		return list;
 		
 		
+	}
+	
+	/*
+	 * Method for handling post requests to add new account
+	 */
+	@POST
+	//@Produces(MediaType.TEXT_HTML)
+	//@Path("/create")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void newAccount(@FormParam("name") String name,@FormParam("balance") float balance,@Context HttpServletResponse servletResponse)throws IOException
+	{
+		
+		
+		System.out.println("\n\nReceived "+name+" "+balance+" Chal jaa yaaaar\n\n");
+		Configuration cfg = new Configuration();
+		cfg.configure("/resources/hibernate.cfg.xml");
+		
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		Session session = factory.openSession();
+		
+		Transaction transaction = session.beginTransaction();
+		
+		Account acc = new Account();
+		acc.setName(name);
+		acc.setBalance(balance);
+		session.persist(acc);
+		transaction.commit();
+		session.close();
+		servletResponse.sendRedirect("http://localhost:8080/Example1/index.html");
 	}
 
 }
